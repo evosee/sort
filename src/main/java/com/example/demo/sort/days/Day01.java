f753c4d454ebd559bbc234843f3cac7ba28e41a9
chensai <chensai123>
2018-10-30 16:59:20 +0800

    sort

--- src/main/java/com/example/demo/sort/days/Day01.java
+++ src/main/java/com/example/demo/sort/days/Day01.java
@@ -7,7 +7,9 @@ public class Day01 {
         int[] array = {10, 9, 112, 10, 1, 2, 7, 113};
         //  bubbleSort(array);
         //selectSort(array);
-        insertSort(array);
+        //insertSort(array);
+        quickSort(array,0,array.length-1);
+      //  sort(array,0,array.length-1);
         System.out.println(Arrays.toString(array));
 
 
@@ -64,6 +66,65 @@ public class Day01 {
             source[left+1] = temp; //在left+1这个位置上设置值
         }
 
+    }
+
+    public static void quickSort(int[] source,int start,int end){
+        int lo = start;
+        int hi = end;
+        int key = source[start];
+        while (start<end){
+            while (start<end&&source[end]>=key) end--;
+            if(key>source[end]){
+                int temp = source[start];
+                source[start] = source[end];
+                source[end] = temp;
+            }
+            while (start<end&&source[start]<=key) start++;
+
+            if(key<source[start]){
+                int temp = source[start];
+                source[start] = source[end];
+                source[end] = temp;
+            }
+
+        }
+
+
+        if(lo<start) quickSort(source,lo,start-1);//左边序列。第一个索引位置到关键值索引-1
+        if(hi>end) quickSort(source,end+1,hi);//右边序列。从关键值索引+1到最后一个
+
+
+
+    }
+
+    public static void sort(int[] a,int low,int high){
+        int start = low;
+        int end = high;
+        int key = a[low];
+
+
+        while(end>start){
+            //从后往前比较
+            while(end>start&&a[end]>=key)  //如果没有比关键值小的，比较下一个，直到有比关键值小的交换位置，然后又从前往后比较
+                end--;
+            if(a[end]<=key){
+                int temp = a[end];
+                a[end] = a[start];
+                a[start] = temp;
+            }
+            //从前往后比较
+            while(end>start&&a[start]<=key)//如果没有比关键值大的，比较下一个，直到有比关键值大的交换位置
+                start++;
+            if(a[start]>=key){
+                int temp = a[start];
+                a[start] = a[end];
+                a[end] = temp;
+            }
+            //此时第一次循环比较结束，关键值的位置已经确定了。左边的值都比关键值小，右边的值都比关键值大，但是两边的顺序还有可能是不一样的，进行下面的递归调用
+        }
+        //递归
+        if(start>low) sort(a,low,start-1);//左边序列。第一个索引位置到关键值索引-1
+        if(end<high) sort(a,end+1,high);//右边序列。从关键值索引+1到最后一个
     }
 }
 
