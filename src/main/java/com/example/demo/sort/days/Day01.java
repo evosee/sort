ab9057c8721fd640c257be97deece2ab0f07eb9c
chensai <chensai123>
2018-10-30 17:10:49 +0800

    sort

--- src/main/java/com/example/demo/sort/days/Day01.java
+++ src/main/java/com/example/demo/sort/days/Day01.java
@@ -68,6 +68,13 @@ public class Day01 {
 
     }
 
+    /**
+     * 快速排序
+     * 思想：用一个初始值来从前往后（拿第一个比他大的交换），和从后往前比（拿第一个比他小的交换）
+     * 这样数组中他右边的都比他大，左边的都比他小。
+     * 然后递归排序
+     *
+     * */
     public static void quickSort(int[] source,int start,int end){
         int lo = start;
         int hi = end;
@@ -97,35 +104,7 @@ public class Day01 {
 
     }
 
-    public static void sort(int[] a,int low,int high){
-        int start = low;
-        int end = high;
-        int key = a[low];
 
-
-        while(end>start){
-            //从后往前比较
-            while(end>start&&a[end]>=key)  //如果没有比关键值小的，比较下一个，直到有比关键值小的交换位置，然后又从前往后比较
-                end--;
-            if(a[end]<=key){
-                int temp = a[end];
-                a[end] = a[start];
-                a[start] = temp;
-            }
-            //从前往后比较
-            while(end>start&&a[start]<=key)//如果没有比关键值大的，比较下一个，直到有比关键值大的交换位置
-                start++;
-            if(a[start]>=key){
-                int temp = a[start];
-                a[start] = a[end];
-                a[end] = temp;
-            }
-            //此时第一次循环比较结束，关键值的位置已经确定了。左边的值都比关键值小，右边的值都比关键值大，但是两边的顺序还有可能是不一样的，进行下面的递归调用
-        }
-        //递归
-        if(start>low) sort(a,low,start-1);//左边序列。第一个索引位置到关键值索引-1
-        if(end<high) sort(a,end+1,high);//右边序列。从关键值索引+1到最后一个
-    }
 }
 
 
