a869708b2679c3e2f81f0c3fb926e0b4fac82ca5
chensai <chensai123>
2018-11-30 20:23:37 +0800

    sort

---
+++ src/main/java/com/example/demo/sort/days/Day29.java
@@ -0,0 +1,83 @@
+package com.example.demo.sort.days;
+
+
+import java.util.Arrays;
+
+/**
+ * @Author: chensai
+ * @Date: 2018/11/30 20:10
+ * @Version 1.0
+ */
+public class Day29 {
+
+    public static void main(String[] args) {
+        int[] a = {2,1,2,1,4,5,4};
+        //bubbleSort(a);
+       // selectSort(a);
+        //insertSort(a);
+        quickSort(a,0,a.length-1);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void bubbleSort(int[] a){
+        for(int i=0;i<a.length-1;i++){
+            for(int j=0;j<a.length-1-i;j++){
+                if(a[j]>a[j+1]){
+                    int t = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = t;
+                }
+            }
+        }
+    }
+
+    public static void selectSort(int[] a){
+        for(int i=0;i<a.length-1;i++){
+            int lo = i;
+            for(int j=i+1;j<a.length;j++){
+                if(a[lo]>a[j]){
+                    lo = j;
+                }
+            }
+            int t = a[lo];
+            a[lo] = a[i];
+            a[i] = t;
+        }
+    }
+
+    public static void insertSort(int[] a){
+        for(int i=1;i<a.length;i++){
+            int left = i-1;
+            int k = a[i];
+            while (left >= 0 && a[left] > k) {
+                a[left+1] = a[left];
+                left--;
+            }
+            a[left+1] = k;
+        }
+    }
+
+    public static void quickSort(int[] a,int start,int end){
+        int key = a[start];
+        int lo = start;
+        int hi = end;
+
+        while (start<end){
+            while (start<end&&a[end]>=key) end--;
+            if(a[end]<key){
+                int t = a[start];
+                a[start] = a[end];
+                a[end] = t;
+            }
+
+            while (start<end&&a[start]<=key) start++;
+            if(a[start]>key){
+                int t = a[start];
+                a[start] = a[end];
+                a[end] = t;
+            }
+        }
+        if(lo<start) quickSort(a,lo,start-1);
+        if(end<hi) quickSort(a,end+1,hi);
+    }
+}
