415449419754dc12cb3ac52e8f021c0f734d2c52
chensai <chensai123>
2019-10-31 15:50:00 +0800

    sort

--- src/main/java/com/example/demo/sort/days/Day208.java
+++ src/main/java/com/example/demo/sort/days/Day208.java
@@ -14,7 +14,7 @@ public class Day208 {
         //selectSort(a);
        // insertSort(a);
         quickSort(a,0,a.length-1);
-        System.out.println(binarySearch(a,0,a.length,223232` `));
+        System.out.println(binarySearch(a,0,a.length,223232));
         System.out.println(Arrays.toString(a));
     }
     public static void bubbleSort(int[] a){
