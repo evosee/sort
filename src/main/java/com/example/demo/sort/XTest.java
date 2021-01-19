98cc8995e6b2dd239e7d0a71461a08248410f815
chensai <chensai123>
2021-01-19 11:04:00 +0800

    commit

---
+++ src/main/java/com/example/demo/sort/XTest.java
@@ -0,0 +1,15 @@
+package com.example.demo.sort;
+
+/**
+ * @author chensai
+ * @Description
+ * @Date 2021/1/18 16:09
+ */
+public class XTest {
+    public static void main(String[] args) {
+        Boolean f = null;
+        if(!f){
+            System.out.println(1);
+        }
+    }
+}
