baea5c2f7146e5f720836ee52e978571a4615753
chensai <chen_sai_@126.com>
2022-02-09 15:11:46 +0800

    commit

--- src/main/java/com/example/demo/sort/thread/Philosopher.java
+++ src/main/java/com/example/demo/sort/thread/Philosopher.java
@@ -1,9 +1,13 @@
 package com.example.demo.sort.thread;
 
 import lombok.Data;
+import lombok.SneakyThrows;
+
+import java.util.concurrent.Semaphore;
 
 /**
  * @author chensai
+ * @Date 2022/2/9 9:44
  * @Description  哲学家问题
  * 5个哲学家共用一张圆桌，分别坐在周围的5张椅子上，
  *
@@ -16,20 +20,30 @@ import lombok.Data;
  * 进餐完毕，放下筷子继续进行思考。
  *
  * 假如5位哲学家同时饥饿，各自拿起左边的筷子时，再去拿各自右边的筷子，因为无筷子可拿而陷入无期限等待（死锁）。
- * @Date 2022/2/9 9:44
+ *
+ *
+ *
  */
 public class Philosopher implements Runnable{
 
     private Integer pos;
+    private Semaphore semaphore;
 
-    public Philosopher(Integer pos){
+    public Philosopher(Integer pos, Semaphore semaphore){
         this.pos = pos;
+        this.semaphore = semaphore;
     }
+    /**
+     * 五个人全部加锁的话会死锁，一直都在等待别人释放，最多拿起4把筷子
+     * */
+    @SneakyThrows
     @Override
     public void run() {
         while (true){
+            semaphore.acquire();
             if(pos==0){
                 synchronized (Chopsticks.getByPos(4)){
+                    System.out.println("拿到了左边");
                     synchronized (Chopsticks.getByPos(pos)){
                         System.out.println(pos+":eating");
                     }
@@ -38,11 +52,13 @@ public class Philosopher implements Runnable{
                 Chopsticks left = Chopsticks.getByPos(pos-1);
                 Chopsticks right = Chopsticks.getByPos(pos);
                 synchronized (left){
+                    System.out.println("拿到了左边");
                     synchronized (right){
                         System.out.println(pos+":eating");
                     }
                 }
             }
+            semaphore.release();
         }
 
 
@@ -66,11 +82,12 @@ public class Philosopher implements Runnable{
     }
 
     public static void main(String[] args) {
-        Thread one = new Thread(new Philosopher(0));
-        Thread two = new Thread(new Philosopher(1));
-        Thread three = new Thread(new Philosopher(2));
-        Thread four = new Thread(new Philosopher(3));
-        Thread five = new Thread(new Philosopher(4));
+        Semaphore semaphore = new Semaphore(4);
+        Thread one = new Thread(new Philosopher(0,semaphore));
+        Thread two = new Thread(new Philosopher(1,semaphore));
+        Thread three = new Thread(new Philosopher(2,semaphore));
+        Thread four = new Thread(new Philosopher(3,semaphore));
+        Thread five = new Thread(new Philosopher(4,semaphore));
         one.start();
         two.start();
         three.start();
